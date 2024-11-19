0bcf5fbb035d937996c45cd191748c69a07adafa
heri1-droid <heri.cyber186@gmail.com>
2024-11-19 08:50:09 +0700

    rra

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,4 +36,29 @@ class AuthController extends Controller
 
         return redirect('/auth');
     }
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])) {
+            $user = Auth::user();
+            if ($user->role == 'admin') {
+                $success['token'] = $user->createToken(
+                    'MDPApp',
+                    ['create', 'read', 'update', 'delete']
+                )->plainTextToken;
+            } else {
+                $success['token'] = $user->createToken(
+                    'MDPApp',
+                    ['read']
+                )->plainTextToken;
+            }
+            $success['name'] = $user->name;
+            return response()->json($success, 201);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    }
 }
