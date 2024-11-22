9de010a36ca565554f21295091bf1b4cb2b08a35
heri1-droid <heri.cyber186@gmail.com>
2024-11-22 09:57:28 +0700

    a

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,41 +37,24 @@ class AuthController extends Controller
         return redirect('/auth');
     }
 
-    // public function login(Request $request)
-    // {
-    //     if (Auth::attempt([
-    //         'email' => $request->email,
-    //         'password' => $request->password
-    //     ])) {
-    //         $user = Auth::user(); // ambil data user dari tabel users sesuai dengan email dan pass
-    //         if ($user->role == 'admin') {
-    //             $success['token'] = $user->createToken('MDPApp', ['create', 'read', 'update', 'delete'])->plainTextToken; // buat token
-    //         } else {
-    //             $success['token'] = $user->createToken('MDPApp', ['read'])->plainTextToken; // buat token
-    //         }
-
-    //         $success['name'] = $user->name; // response nama user
-    //         return response()->json($success, 201);
-    //     } else {
-    //         return response()->json(['error' => 'Unauthorized'], 401);
-    //     }
-    // }
     public function login(Request $request)
     {
         if (Auth::attempt([
             'email' => $request->email,
             'password' => $request->password
         ])) {
-            $user = Auth::user();
-            if ($user->role == 'A') {
-                $success['token'] = $user->createToken('MDPApp', ['create-fakultas', 'read-fakultas', 'update-fakultas', 'delete-fakultas'])->plainTextToken;
+            $user = Auth::user(); // ambil data user dari tabel users sesuai dengan email dan pass
+            if ($user->role == 'admin') {
+                $success['token'] = $user->createToken('MDPApp', ['create', 'read', 'update', 'delete'])->plainTextToken; // buat token
             } else {
-                $success['token'] = $user->createToken('MDPApp', ['read-fakultas'])->plainTextToken;
+                $success['token'] = $user->createToken('MDPApp', ['read'])->plainTextToken; // buat token
             }
-            $success['name'] = $user->name;
+
+            $success['name'] = $user->name; // response nama user
             return response()->json($success, 201);
         } else {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
     }
+
 }
